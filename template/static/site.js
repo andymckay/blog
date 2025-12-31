e22f2ac3871f3f35290a1d8f0004067bf746a9e5
Andy McKay <74699+andymckay@users.noreply.github.com>
2025-12-30 19:57:59 -0800

    add theme switcher

--- template/static/site.js
+++ template/static/site.js
@@ -9,7 +9,31 @@ window.addEventListener("load", (event) => {
             ride: 'carousel',
         });
     }
+
     for (let el of document.getElementsByClassName("strava-explanation")) {
         el.style.display = "none";
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    function switchTheme(event) {
+        let html = document.getElementsByTagName("html")[0];
+        let theme = html.getAttribute("data-bs-theme") === "dark" ? "light" : "dark";
+        html.setAttribute("data-bs-theme", theme);
+        localStorage.setItem("theme", theme);
+        event.preventDefault();
+    }
+
+
+    for (let el of document.getElementsByClassName("theme-switch")) {
+        el.addEventListener("click", switchTheme);
+    }
+
+    let theme = localStorage.getItem("theme");
+    if (!theme) {
+        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+    if (theme) {
+        document.getElementsByTagName("html")[0].setAttribute("data-bs-theme", theme);
     }
 });
