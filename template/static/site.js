f7e540f5a46a3314e1f4166bcca185ce2a96d8e3
Andy McKay <74699+andymckay@users.noreply.github.com>
2025-12-30 17:31:09 -0800

    update js

--- template/static/site.js
+++ template/static/site.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", (event) => {
     const carouselElement = document.getElementById("carousel");
-    if (0) { //carouselElement) {
+    if (carouselElement) {
         const carousel = new bootstrap.Carousel(carouselElement, {
             interval: 5000,
             wrap: true,
@@ -11,22 +11,5 @@ window.addEventListener("load", (event) => {
     }
     for (let el of document.getElementsByClassName("strava-explanation")) {
         el.style.display = "none";
-    }
-
-    function changeScreen(event) {
-        let carouselElement = event.target.parentNode.parentNode;
-        carouselElement.classList.toggle("modal-fullscreen");
-        if (event.target.text == "Bigger") {
-            event.target.text = "Smaller";
-        } else {
-            event.target.text = "Bigger";
-        }
-        //event.preventDefault();
-    }
-
-    for (let el of document.getElementsByClassName("carousel-enlarge")) {
-        for (let a of el.getElementsByTagName("a")) {
-            a.addEventListener("click", changeScreen);
-        }
     }
 });
